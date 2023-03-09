91099dadbb89b92fd6be9960639b01210f13e428
Сергей Челпанов <S.inter.4@mail.ru>
2023-03-09 18:12:25 +0300

    Вывод массива на экран

--- Program.cs
+++ Program.cs
@@ -25,3 +25,38 @@ int SearchValidSizeArray(string[] fullArray, int lengthElement)
     return validSizeArray;
 }
 
+// Mассив из строк, длина которых меньше либо равна заданному количеству символов 
+string[] GetLimitLengthElementArray(string[] fullArray, int lengthElement, int sizeArray)
+{
+    string[] resultArray = new string[sizeArray];
+    string element = string.Empty;
+    int count = 0;
+    for (int i = 0; i < resultArray.Length; i++)
+    {
+        for (int j = count; j < fullArray.Length; j++)
+        {
+            string currentElement = fullArray[j];
+            if (currentElement.Length <= lengthElement)
+            {
+                element = currentElement;
+                count = j;
+                break;
+            }
+            else count += j;
+        }
+        resultArray[i] = element;
+        count++;
+    }
+    return resultArray;
+}
+
+// вывожу масивы на экран 
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
